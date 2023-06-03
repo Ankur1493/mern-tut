4b79c9ef9ffc21721dba709b93dd5166d5db4fb7
Ankur Sharma <ankursharma1493@gmail.com>
2023-06-04 02:14:46 +0530

    added functionality for delete and update controller

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -52,25 +52,59 @@ exports.createNewWorkout = async (req,res)=>{
 
 //delete workout
 
-exports.deleteWorkout  = (req,res)=>{
+exports.deleteWorkout  = async (req,res)=>{
 
-    const id = req.params.id*1;
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({
+            error: "wrong id, no such workout"
+        })
+    }
+    
+    const workout = await Workout.findOneAndDelete({_id: id});
+    
+    if(!workout){
+        return res.status(404).json({
+            status: "fail",
+            message: "no such workout"
+        })
+    }
     res.status(200).json({
-        id,
         status: "success",
-        message: "you have deleted this workout"
+        message: "you have deleted this workout",
+        data: {
+            workout
+        }
     })
 
 }
 
 //update workout
-exports.updateWorkout  = (req,res)=>{
+exports.updateWorkout  = async (req,res)=>{
 
-    const id = req.params.id*1;
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({
+            error: "wrong id, no such workout"
+        })
+    }
+    const workout = await Workout.findByIdAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if(!workout){
+        return res.status(404).json({
+            status: "fail",
+            message: "no such workout"
+        })
+    }
+
     res.status(200).json({
-        id,
         status: "success",
-        message: "you have updated this workout"
+        message: "you have updated this workout",
+        data: {
+            workout
+        }
     })
 
 }
