b47d62566e956c4c63e655a4a45cb3f916e3812e
Ankur Sharma <ankursharma1493@gmail.com>
2023-06-04 02:02:16 +0530

    created a new file for controllers and added functionality for all,single and creation

---
+++ backend/controllers/controllers.js
@@ -0,0 +1,79 @@
+const { default: mongoose } = require("mongoose");
+const Workout = require("../models/workoutStructure")
+
+//get all workouts
+exports.getAllWorkOuts = async (req,res)=>{
+
+    const workouts = await Workout.find({}).sort({createdAt: -1});
+
+    res.status(200).json(workouts)
+}
+
+//get single workout
+exports.getSingleWorkout = async (req,res)=>{
+
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({
+            error: "wrong id, no such workout"
+        })
+    }
+
+    const workout = await Workout.findById(id)
+    if(!workout){
+        return res.status(404).json({error: "No such workout"});
+    }
+    res.status(200).json({
+        status: "success",
+        data:{
+            workout
+        }
+    })
+
+}
+
+//create a workout
+exports.createNewWorkout = async (req,res)=>{
+    const {title,load,reps} = req.body;
+
+    try{
+        const workout = await Workout.create({title,load,reps})
+        res.status(200).json(workout)
+    }catch(error){
+        res.status(400).json({
+            error,
+            status: "failed",
+            message: "failure failure"
+        })
+    }
+    
+}
+
+//delete workout
+
+exports.deleteWorkout  = (req,res)=>{
+
+    const id = req.params.id*1;
+    res.status(200).json({
+        id,
+        status: "success",
+        message: "you have deleted this workout"
+    })
+
+}
+
+//update workout
+exports.updateWorkout  = (req,res)=>{
+
+    const id = req.params.id*1;
+    res.status(200).json({
+        id,
+        status: "success",
+        message: "you have updated this workout"
+    })
+
+}
+
+
+
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,60 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Workout = require("../models/workoutStructure")
+const controllers = require("../controllers/controllers");
 
-
-const getAllWorkOuts = (req,res)=>{
-    res.status(200).json({
-        message: "you have accessed all routes"
-    })
-}
-const createNewWorkout = async (req,res)=>{
-    const {title,load,reps} = req.body;
-
-    try{
-        const workout = await Workout.create({title,load,reps})
-        res.status(200).json(workout)
-    }catch(error){
-        res.status(400).json({
-            error,
-            status: "failed",
-            message: "failure failure"
-        })
-    }
-    
-}
-
-const getSingleWorkout  = (req,res)=>{
-
-    const id = req.params.id*1;
-    res.status(200).json({
-        id,
-        status: "success",
-        message: "you have gotten this single workout"
-    })
-
-}
-const deleteWorkout  = (req,res)=>{
-
-    const id = req.params.id*1;
-    res.status(200).json({
-        id,
-        status: "success",
-        message: "you have deleted this workout"
-    })
-
-}
-const updateWorkout  = (req,res)=>{
-
-    const id = req.params.id*1;
-    res.status(200).json({
-        id,
-        status: "success",
-        message: "you have updated this workout"
-    })
-
-}
-
+const {getAllWorkOuts,getSingleWorkout,createNewWorkout,deleteWorkout,updateWorkout} = controllers;
 
 router.route("/")
     .get(getAllWorkOuts)
