b254a04caf723aa15120518b3ba0e506679c69f9
Ankur Sharma <ankursharma1493@gmail.com>
2023-06-04 01:14:35 +0530

    connection established between mongodb and express app

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose")
 const workoutRouters = require("./routes/workouts")
 //express app created
 const app = express();
 
 //middleware
 app.use((req,res,next)=>{
-
     console.log(req.path, req.method);
     next();
-
 })
 
 //this below middleware is used to get the body from requests
@@ -17,8 +16,12 @@ app.use(express.json());
 
 app.use("/api/workouts",workoutRouters)
 
-//listening for requests
-const port = process.env.PORT
-app.listen(port, ()=>{
-    console.log(`listening on port: ${port} `);
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listening for requests
+        const port = process.env.PORT
+        app.listen(port, ()=>{
+            console.log(`connected to database listening on port: ${port} `);
+        })        
+    })
+    .catch((err)=>{console.log(err);})
